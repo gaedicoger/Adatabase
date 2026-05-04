2b260f813d79e61af5b2bab523f3e99fdfab1538
Gaed <oger.gaedic@gmail.com>
2026-05-04 16:04:30 +0200

    creation des tables

---
+++ migration_up.sql
@@ -0,0 +1,68 @@
+-- CREATION DES TABLES DANS L'ORDRE LOGIQUE:
+
+-- Créer les données énumérées resource_type:
+CREATE TYPE resource_type AS ENUM ('guide', 'video', 'exercice', 'projet');
+
+-- Créer la table themes :
+CREATE TABLE IF NOT EXISTS  public.themes
+(
+    id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
+    name TEXT,
+    description TEXT,
+    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE IF EXISTS public.themes
+    OWNER to admin;
+
+-- Créer la table skills:
+CREATE TABLE IF NOT EXISTS public.skills
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
+    name text,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE IF EXISTS public.skills
+    OWNER to admin;
+
+-- Créer la table principale pour les ressources pédagogiques:
+CREATE TABLE IF NOT EXISTS public.resources
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
+    type resource_type,
+    title text,
+    description text,
+    url text,
+    is_ada boolean,
+    theme_id integer,
+    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    CONSTRAINT "theme id key" FOREIGN KEY (theme_id)
+        REFERENCES public.themes (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+);
+
+ALTER TABLE IF EXISTS public.resources
+    OWNER to admin;
+
+-- Créer la table de liaisons resources_skills :
+CREATE TABLE IF NOT EXISTS public.resources_skills
+(
+    resource_id integer ,
+    skill_id integer,
+    CONSTRAINT "resource id key" FOREIGN KEY (resource_id)
+        REFERENCES public.resources (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE,
+    CONSTRAINT "skill id key" FOREIGN KEY (skill_id)
+        REFERENCES public.skills (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
+);
+
+ALTER TABLE IF EXISTS public.resources_skills
+    OWNER to admin;
