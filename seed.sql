2f08c159e918652db762de7bf03440b1b25fefc3
Gaed <oger.gaedic@gmail.com>
2026-05-06 11:37:00 +0200

    Mettre les données seed

--- seed.sql
+++ seed.sql
@@ -12,7 +12,9 @@ INSERT INTO resources (title, url, description, type, is_ada, theme_id) VALUES
   ('Introduction à React',	'https://react.dev',	'Guide officiel React', 'guide',	false,	1),
   ('SQL pour débutants',	'https://example.com/sql',	'Cours SQL complet', 'video',	false,	3),
   ('Exercices JavaScript',	'https://example.com/js',	'Pratique JS', 'exercice',	false,	1),
-  ('Créer une API en node',	'https://example.com/node',	'Projet node	projet', 'projet',	false,	2);
+  ('Créer une API en node',	'https://example.com/node',	'Projet node	projet', 'projet',	false,	2),
+  ('Débuter Python',	'https://example.com/python',	'Cours Python débutant', 'guide',	true,	2),
+  ('HTML / CSS Documentation',	'https://example.com/html',	'Documentation complète HTML/CSS', 'guide',	false,	1);
 
 --Insérer les données pour Skills :
 INSERT INTO skills (name) VALUES
@@ -20,7 +22,9 @@ INSERT INTO skills (name) VALUES
   ('React'),
   ('SQL'),
   ('PostgreSQL'),
-  ('Node.js');
+  ('Node.js'),
+  ('Python'),
+  ('HTML/CSS');
 
 --Insérer les données pour Ressources_skills table de liaison :
 
@@ -31,4 +35,6 @@ INSERT INTO resources_skills (resource_id, skill_id) VALUES
   (2,	4),
   (3,	1),
   (4,	1),
-  (4,	5);
+  (4,	5),
+  (5,	6),
+  (6,	7);
