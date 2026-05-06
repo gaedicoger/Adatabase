0a8075045fb6b1a251fea5b9db5e65a75ffb11dd
Gaed <oger.gaedic@gmail.com>
2026-05-06 15:24:39 +0200

    supression drop if exists

--- migration_down.sql
+++ migration_down.sql
@@ -1,15 +1,15 @@
 --Créer un script pour supprimer toutes les tables :
 --Attention bien supprimer les tables selon l'ordre inverse de création
 
-DROP TABLE IF EXISTS public.resources_skills;
+DROP TABLE public.resources_skills;
 
-DROP TABLE IF EXISTS public.skills;
+DROP TABLE public.skills;
 
-DROP TABLE IF EXISTS public.resources;
+DROP TABLE public.resources;
 
-DROP TABLE IF EXISTS public.themes;
+DROP TABLE public.themes;
 
-DROP TYPE IF EXISTS public.resource_type;
+DROP TYPE public.resource_type;
 
 
 
